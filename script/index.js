1ae316551f883e2670af43ea588b8e65bf20c6e2
jooaosanntos <101375975+jooaosanntos@users.noreply.github.com>
2022-07-14 01:11:41 -0300

    Segundo commit
    Tarefas realizadas:
    - As tasks agora são arrastáveis e dropáveis

--- script/index.js
+++ script/index.js
@@ -1,7 +1,18 @@
+
 // Fazer menu de criação de task aparecer
 $("#adicionar-task").on("click", () => {
     $(".menu-task").css("display", "block")
 })
+
+// Trecho de código que gera um id para uma task
+const randomValue = {
+    _id: 0,
+    get id(){
+        return this._id++
+    }
+}
+
+const gerarId = () => "task-" + randomValue.id
 
 // Fazer menu de criação de task desaparecer
 const fecharMenuTask = () => $(".menu-task").css("display", "none")
@@ -13,7 +24,12 @@ function criarTask(titulo, conteudo){
     const tituloElem = $("<h2>").text(titulo)
     const conteudoElem = $("<p>").text(conteudo)
     const div = $("<div>").addClass("task").append(tituloElem, conteudoElem)
-
+    div.get(0).draggable = true
+    div.attr("id", gerarId())
+    
+    div.get(0).ondragstart = event => {
+        event.dataTransfer.setData("item-id", event.target.id)
+    }
     return div
 }
 
@@ -27,4 +43,21 @@ $("#menu-task-adicionar").on("click", (event) => {
     $(".container-left").append(criarTask(titulo, conteudo))
 
     fecharMenuTask()
+})
+
+$(".task").each((index, task) => {
+    task.draggable = true
+    task.id = task.id || gerarId()
+
+    task.ondragstart = event => {
+        event.dataTransfer.setData("item-id", event.target.id)
+    }
+})
+
+$(".dropzone").each((index, dropzone) => {
+    dropzone.ondragover = event => event.preventDefault()
+    dropzone.ondrop = function(event){
+        const id = event.dataTransfer.getData("item-id")
+        dropzone.appendChild(document.getElementById(id))
+    }
 })
